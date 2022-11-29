18b28bcd8e5e04768fc8fdd116a8e5ad44f8558f
serg <sergej-gubskij@mail.ru>
2022-11-29 15:39:03 +0600

    get_fine_choice(some_choice) added

--- 002/rsp_utils.py
+++ 002/rsp_utils.py
@@ -5,6 +5,7 @@ from settings import *
 
 # possible_actions = ["rock", "paper", "scissors"]
 possible_actions = []
+possible_actions2 = {}
 user_name = ""
 game_status_model = []
 
@@ -35,11 +36,20 @@ def load_game_model():
     game_status_model = load_json_object(get_model_file_location())
 
     global possible_actions
+    global possible_actions2
     possible_actions = []
     for status in game_status_model:
-        possible_actions.append(status.get('name'))
+        sn = status.get('name')
+        possible_actions.append(sn)
+        possible_actions2[sn] = sn
+        for short in status.get('shorts'):
+            possible_actions.append(short)
+            possible_actions2[short] = sn
 
-
+def get_fine_choice(choice)->str:
+    # return possible_actions2[choice]
+    return possible_actions2.get(choice,'')
+            
 def define_user() -> str:
     return input("Enter your name: ")
 
@@ -49,7 +59,7 @@ def get_user_input() -> str:
     for status in game_status_model:
         if len(s) > 0:
             s += ', '
-        s = s + status.get('name')
+        s = s + status.get('name') + '('+str(status.get('shorts'))+')'
 
     return input("Enter a choice (" + s + "): ")
 
@@ -59,22 +69,26 @@ def check_choice_valid(choice) -> bool:
 
 
 def get_computer_choice():
-    return random.choice(possible_actions)
+    # return random.choice(possible_actions)
+    return random.choice(game_status_model).get('name')
 
 
 def show_play_result():
     print("show_play_result")
 
+def check_status_equals(status, user_choice) ->bool:
+    return status.get('name') == user_choice or user_choice in status.get('shorts')
 
 def determine_result_2(user_choice, computer_choice):
     for status in game_status_model:
-        if (status.get('name') == user_choice):
+        # if (status.get('name') == user_choice):
+        if (check_status_equals(status, user_choice)):
             if (computer_choice in status.get('win')):
-                print('User1 win: '+user_choice+' vs '+computer_choice)
+                print('User1 win: '+get_fine_choice(user_choice)+' vs '+get_fine_choice(computer_choice))
             elif (computer_choice in status.get('lose')):
-                print('User2 win: '+user_choice+' vs '+computer_choice)
+                print('User2 win: '+get_fine_choice(user_choice)+' vs '+get_fine_choice(computer_choice))
             else:
-                print(f"Both players selected {user_choice}. It's a tie!\n")
+                print(f"Both players selected {get_fine_choice(user_choice)}. It's a tie!\n")
 
             break
 
@@ -104,14 +118,14 @@ def play():
 
     while True:
 
-        user_action = get_user_input()
+        user_choice = get_user_input()
 
-        if check_choice_valid(user_action):
-            computer_action = get_computer_choice()
+        if check_choice_valid(user_choice):
+            computer_choice = get_computer_choice()
             print(
-                f"\nYou chose {user_action}, computer chose {computer_action}.")
+                f"\nYou chose {user_choice}, computer chose {computer_choice}.")
             # determine_result(user_action, computer_action)
-            determine_result_2(user_action, computer_action)
+            determine_result_2(user_choice, computer_choice)
 
         else:
             print(f"{user_name}, please, enter the correct form word.")
